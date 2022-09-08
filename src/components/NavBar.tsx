50be5817d40a353b131daa8b62c2a3c9d644644e
Jason171096 <bboyace10@gmail.com>
2022-09-08 00:13:57 -0500

    Toggle with animation

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import menuIco from '@icons/menu.svg';
 import { Link } from 'gatsby'
 
 const NavBar = () => {
+    const [openSide, setOpenSide] = useState(true);
     const styleLi = "inline hover:text-redKing transition delay-75 cursor-none";
-    const openNav = () => {
-        document.getElementById("sideNav").style.width = "100%"
+    const styleSpan = "absolute bg-white w-full h-1 rounded transition duration-500";
+    const onClickToggle = () => {
+        setOpenSide(!openSide)
+        const sideNav = document.getElementById("sideNav")
+        const span1 = document.getElementById("span1")
+        const span2 = document.getElementById("span2")
+        const span3 = document.getElementById("span3")
+
+        if(openSide) {
+            span1.classList.add("-rotate-45");
+            span1.classList.add("top-[13px]");
+
+            span3.classList.add("rotate-45");
+            span3.classList.add("bottom-[15px]");
+
+            span2.classList.add("opacity-0");
+            sideNav.style.width = "100%"
+        }
+        else {
+            span1.classList.remove("-rotate-45");
+            span1.classList.remove("top-[13px]");
+
+            span3.classList.remove("rotate-45");
+            span3.classList.remove("bottom-[15px]");
+
+            span2.classList.remove("opacity-0");
+            sideNav.style.width = "0%"
+        }
+
+        
+
     }
     return (
         <div className="relative ml-auto pr-5 py-7 text-white font-bold text-2xl lg:text-3xl">
@@ -15,8 +45,10 @@ const NavBar = () => {
                 <li className={styleLi}><Link to="/proyectos">Proyectos</Link></li>
                 <li className={styleLi}><Link to="/contacto">Contacto</Link></li>
             </ul>
-            <div onClick={openNav} className="w-10 h-10 sm:hidden">
-                <img src={menuIco}/>
+            <div onClick={onClickToggle} className="w-10 h-8 relative z-20 sm:hidden">
+                <span id="span1" className={styleSpan + " top-0"}></span>
+                <span id="span2" className={styleSpan + " top-[14px]"} ></span>
+                <span id="span3" className={styleSpan + " bottom-0"}></span>
             </div>
         </div>
     )
--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -3,14 +3,10 @@ import closeIco from '@icons/close.svg';
 import { Link } from 'gatsby'
 
 const SideNav = () => {
-    const closeNav = () => {
-        document.getElementById("sideNav").style.width = "0%"
-    }
     return (
         <div id="sideNav" className="h-full w-0 fixed z-10 top-0 left-0 overflow-x-hidden transition delay-100 bg-gradient-to-tr from-blueMap to-violetMap">
             <div className="relative pr-5 py-10">
-                <div onClick={closeNav} className="w-7 h-7 ml-auto">
-                <img  src={closeIco}/> 
+                <div className="w-10 h-10 ml-auto">
                 </div>
             </div>
             <div className="flex justify-center items-center text-white text-lg">
